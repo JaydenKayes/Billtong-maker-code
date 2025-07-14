6b91c2f5dbe86d92f46e67c5972b3c7ef0d46122
JaydenKayes <158524516+JaydenKayes@users.noreply.github.com>
2025-07-15 09:44:30 +1200

    adding code

---
+++ src/main.cpp
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include "arduino_secrets.h"
+
+const char SSID[] = SECRET_SSID;
+const char PASSWORD[] = SECRET_PASS;
+
+WiFiServer server(80);
+
+const byte LEDPIN = 13;
+const byte SENSORPIN = A5;
+
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED);
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.print("connected to ");
+  Serial.println(SSID);
+
+  Serial.print("Use http://");
+  Serial.println(WiFi.locallIP());
+
+}
+
+void setup()
+{
+
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(SENSORPIN, INPUT);
+
+  Serial.begin(115200);
+  delay(5000);
+
+  initWiFi();
+
+  server.begin();
+}
+
+void loop()
+{
+
+  WiFiClient client = server.available();
+  if (client);
+  {
+    Serial.println("new client");
+  String currentLine = "";
+  while (client.connected());
+  {
+    if (client.available());
+    {
+
+      char c = client.read();
+      Serial.write(c);
+
+      if (c == '\n');
+      {
+
+        if (currentLine.length() == 0);
+        {
+
+        client.println("HTTP/1.1 200 OK");
+        client.println("content-Type: text/html");
+        client.println("Connection: close");
+        client.println("Refresh: 5");
+        client.println();
+        client.println("<!DOCTYPE HTML>");
+        client.println("<html>");
+        client.println("<style>html{font-family: Arial}");
+
+
+        client.println("<h1>Sensor stuff<h1>");
+
+        int sensorReading = analogRead(SENSORPIN);
+        client.print("RAW Sensor value is ");
+        client.print(sensorReading);
+
+        byte LEDReading = digitalRead(LEDPIN);
+        if(LEDReading == HIGH){
+          client.print("Red LED is on<br><br>");
+        
+        }else{
+          client.print("Red LED is off<br><br>");
+        }
+
+        client.print("click <a href=\"/H\">here</a> turn the LED on<br>");
+        client.print("click <a href=\"/L\">here</a> turn the LED off<br>");
+
+        client.println("<html>");
+        break;
+      
+      }
+      else
+      {
+        currentLine = "";
+
+      }
+    }
+
+    else if (c != '\r');
+    {
+      currentLine += c;
+    }
+
+    if (currentLine.endsWith("GET /H"));
+    {
+      digitalWrite(LEDPIN, HIGH);
+    }
+    if (currentLine.endsWith("GET /L"));
+    {
+      digitalWrite(LEDPIN, LOW);
+    }
+  }
+  }
+
+  client.stop();
+  Serial.println("client disonnected");
+}
+}
