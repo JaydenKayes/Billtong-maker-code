90eb85788915324ed60c70a7213b4b334cfb6748
JaydenKayes <158524516+JaydenKayes@users.noreply.github.com>
2025-08-14 12:34:16 +1200

    save commit to github

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,15 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "arduino_secrets.h" // Contains SSID and PASSWORD
+#include "arduino_secrets.h" // Contains SECRET_SSID and SECRET_PASS
 
 const char SSID[] = SECRET_SSID;
 const char PASSWORD[] = SECRET_PASS;
 
-WiFiServer server(80); // Web server runs on port 80
+WiFiServer server(80);
 
-const int LED_PIN = 13;      // Built-in LED pin
-const int SENSOR_PIN = 34;   // Valid analog pin for ESP32
+const int LED_PIN = 13;
+const int SENSOR_PIN = 34; n 
 
-// Flashes LED on and off for a given number of times
-void flashLED(int interval_ms, int repeatCount) {
-  for (int i = 0; i < repeatCount; i++) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(interval_ms);
-    digitalWrite(LED_PIN, LOW);
-    delay(interval_ms);
-  }
-}
-
-// Connect to WiFi network
 void connectToWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
@@ -35,33 +24,29 @@ void connectToWiFi() {
 }
 
 void setup() {
-  Serial.begin(115200);  // Start serial monitor
+  Serial.begin(115200);
   delay(1000);
 
   pinMode(LED_PIN, OUTPUT);
   pinMode(SENSOR_PIN, INPUT);
 
-  connectToWiFi(); // Start WiFi connection
-  server.begin();  // Start web server
+  connectToWiFi();
+  server.begin();
 }
 
-// Handle a connected web client
 void handleClient(WiFiClient client) {
-  String request = client.readStringUntil('\r'); // Read HTTP request line
+  String request = client.readStringUntil('\r');
   Serial.println(request);
 
-  // LED control via URL
   if (request.indexOf("GET /H") >= 0) {
-    digitalWrite(LED_PIN, HIGH);  // Turn LED on
+    digitalWrite(LED_PIN, HIGH);
   } else if (request.indexOf("GET /L") >= 0) {
-    digitalWrite(LED_PIN, LOW);   // Turn LED off
+    digitalWrite(LED_PIN, LOW);
   }
 
-  // Read sensor and LED state
   int sensorValue = analogRead(SENSOR_PIN);
   bool ledState = digitalRead(LED_PIN);
 
-  // Send basic HTML response
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("Connection: close");
@@ -81,14 +66,12 @@ void handleClient(WiFiClient client) {
   client.println("</body></html>");
   client.flush();
   delay(10);
-  client.stop(); // End connection
+  client.stop();
   Serial.println("Client disconnected");
 }
 
 void loop() {
-  flashLED(500, 5); // Blink LED for feedback
-
-  WiFiClient client = server.available(); // Check for incoming connection
+  WiFiClient client = server.available();
   if (client) {
     Serial.println("New client connected");
     handleClient(client);
